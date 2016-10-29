5a31b1f123846edefd5f9d986670cf5eb1212f3a
hexuan <hexuan@coacocodemac-mini.local>
2016-10-29 21:51:16 +0800

    Initial Commit

---
+++ AudioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioPlayer
+//
+//  Created by hexuan on 16/10/29.
+//  Copyright © 2016年 hexuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioPlayer
+//
+//  Created by hexuan on 16/10/29.
+//  Copyright © 2016年 hexuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
